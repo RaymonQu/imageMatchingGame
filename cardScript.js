a7e9e12a08b176110e4724f0b0ce7897a6ce3262
RaymonQu <dagon7898@gmail.com>
2022-12-04 23:04:35 -0500

    Mostly Finished HTML and CSS

---
+++ cardScript.js
@@ -0,0 +1,27 @@
+function initialize() {
+	if (document.images) {
+		var img1 = new Image();
+		var img2 = new Image();
+		var img3 = new Image();
+        var img4 = new Image();
+		var img5 = new Image();
+		var img6 = new Image();
+		var img7 = new Image();
+        var img8 = new Image();
+        var img9 = new Image();
+
+		img1.src = "images/ai1.png";
+		img2.src = "images/amaz.jpg";
+		img3.src = "images/American_Beaver.jpg";
+		img4.src = "images/city.png";
+		img5.src = "images/nok.PNG";
+		img6.src = "images/sub.jpg";
+		img7.src = "images/wall.jpg";
+		img8.src = "images/cow.jpg";
+        img9.src = "images/cardback.png";
+	}
+}
+
+function flipCard{
+
+}
