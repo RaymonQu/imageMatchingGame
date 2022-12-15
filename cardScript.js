3faf4f7376a080934c25caaf1133d4b01b9a5bee
ray <ray>
2022-12-15 13:49:49 -0500

    made things better and thought about it

--- cardScript.js
+++ cardScript.js
@@ -1,34 +1,45 @@
 function initialize() {
-	cardList = [];
-	for (let i = 0; i < 2; i++) {
-		cardList.push(document.getElementById(`nok${i}`));
+
+	boardImages = [];
+	imagesGenerated = 0;
+	while(imagesGenerated < 16){
+
 	}
 
 	if (document.images) {
-		img1 = new Image();
-		img2 = new Image();
-		img3 = new Image();
-        img4 = new Image();
-		img5 = new Image();
-		img6 = new Image();
-		img7 = new Image();
-        img8 = new Image();
-        img9 = new Image();
+		urlArray = ["images/ai1.png", "images/amaz.jpg", "images/American_Beaver.jpg", "images/city.png",
+					"images/nok.PNG", "images/sub.jpg", "images/wall.jpg", "images/cow.jpg", "images/cardback.png"];
+		images = [];
+		for (let i = 1; i < 10; i++){
+			images.push(new Image().src = urlArray[i]);
+		}
+		// img1 = new Image();
+		// img2 = new Image();
+		// img3 = new Image();
+        // img4 = new Image();
+		// img5 = new Image();
+		// img6 = new Image();
+		// img7 = new Image();
+        // img8 = new Image();
+        // img9 = new Image();
 
-		img1.src = "images/ai1.png";
-		img2.src = "images/amaz.jpg";
-		img3.src = "images/American_Beaver.jpg";
-		img4.src = "images/city.png";
-		img5.src = "images/nok.PNG";
-		img6.src = "images/sub.jpg";
-		img7.src = "images/wall.jpg";
-		img8.src = "images/cow.jpg";
-        img9.src = "images/cardback.png";
+		// img1.src = "images/ai1.png";
+		// img2.src = "images/amaz.jpg";
+		// img3.src = "images/American_Beaver.jpg";
+		// img4.src = "images/city.png";
+		// img5.src = "images/nok.PNG";
+		// img6.src = "images/sub.jpg";
+		// img7.src = "images/wall.jpg";
+		// img8.src = "images/cow.jpg";
+        // img9.src = "images/cardback.png";
 	}
+
 }
 
 function flipCard(a){
-	x = document.getElementById("nok1");
-	x.src = img4.src;
-	console.log(1);
+	a = images.src()
+}
+
+function generateRandomImage(){
+	return 
 }
