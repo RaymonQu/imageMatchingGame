1c883d576f264b003fa869f510b7c14a6a8c89e0
RaymonQu <dagon7898@gmail.com>
2022-12-19 00:44:32 -0500

    javascript isfun

--- cardScript.js
+++ cardScript.js
@@ -1,43 +1,77 @@
 function initialize() {
+	gameover = false;
+	sourceCardFlipped = false;
+	score = 0;
+	lastImageClicked = null;
 	if (document.images) {
 		urlArray = ["images/ai1.png", "images/amaz.jpg", "images/American_Beaver.jpg", "images/city.png",
-					"images/nok.PNG", "images/sub.jpg", "images/wall.jpg", "images/cow.jpg"];
+					"images/nok.PNG", "images/sub.jpg", "images/wall.jpg", "images/cow.jpg", "images/cardback.png"];
 		images = [];
-		for (let i = 1; i < 10; i++){
+		for (let i = 0; i < 9; i++){
 			images.push(new Image().src = urlArray[i]);
 		}
-		// img1 = new Image();
-		// img2 = new Image();
-		// img3 = new Image();
-        // img4 = new Image();
-		// img5 = new Image();
-		// img6 = new Image();
-		// img7 = new Image();
-        // img8 = new Image();
-        // img9 = new Image();
-
-		// img1.src = "images/ai1.png";
-		// img2.src = "images/amaz.jpg";
-		// img3.src = "images/American_Beaver.jpg";
-		// img4.src = "images/city.png";
-		// img5.src = "images/nok.PNG";
-		// img6.src = "images/sub.jpg";
-		// img7.src = "images/wall.jpg";
-		// img8.src = "images/cow.jpg";
-        // img9.src = "images/cardback.png";
 	}
-
 	boardImages = [];
+	randomFlags = [];
+	for (let i = 0; i < 8; i++){
+		randomFlags.push(false);
+	}
+	console.log(randomFlags);
 	imagesGenerated = 0;
+	let a = 7
+	counter = 0;
 	while(imagesGenerated < 16){
-		if()
+		let randNum = generateRandomImageNum(a);
+		console.log(randNum);
+		console.log(images[randNum]);
+		if(!randomFlags[randNum] && typeof images[randNum] !== 'undefined'){
+			randomFlags[randNum] = true;
+			imagesGenerated++;
+			boardImages.push(images[randNum]);
+		}
+		else if (randomFlags[randNum] && typeof images[randNum] !== 'undefined'){
+			boardImages.push(images[randNum]);
+			imagesGenerated++;
+			delete images[randNum];
+		}
+		counter++;
 	}
 }
+
 
 function flipCard(a){
-	a = images.src()
+	if(!gameover){
+
+		console.log(boardImages[a]);
+		console.log("image" + a);
+		firstImage = document.getElementById("image" + a);
+		if(!sourceCardFlipped){
+			firstImage.src = boardImages[a];
+			lastImageClicked = boardImages[a];
+			sourceCardFlipped = true;
+			updateScore();
+		}
+		else if(sourceCardFlipped){
+
+
+			sourceCardFlipped = false;
+		}
+	}
 }
 
-function generateRandomImageNum(){
-	return Math.floor((Math.random() * 8 + 1)) + 1
+function generateRandomImageNum(a){
+	return Math.floor((Math.random() * 8))
+}
+
+function updateScore(){
+	score++;
+	document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function reset(){
+	for(let i = 0; i < 16; i++){
+		document.getElementById("image" + i).src = urlArray[8];
+	}
+	console.log(urlArray[8]);
+	initialize();
 }
